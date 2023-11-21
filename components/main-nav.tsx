79c5d77acaf4834845dea9851cea685a0930e18c
Liron Avraham <lironamy@gmail.com>
2023-11-21 18:08:39 +0200

    added mobile navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 
+import { useState } from 'react';
+import { GrClose, GrMenu } from 'react-icons/gr';
+
 import { cn } from "@/lib/utils"
 import { Category } from "@/types";
 
@@ -10,38 +13,86 @@ interface MainNavProps {
   data: Category[];
 }
 
+
 const MainNav: React.FC<MainNavProps> = ({
   data
 }) => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen((prevOpen) => !prevOpen);
+
+    // Toggle body scroll lock
+    document.body.classList.toggle('overflow-hidden', !open);
+  };
 
   const routes = data.map((route) => ({
     href: `/category/${route.id}`,
     label: route.name,
     active: pathname === `/category/${route.id}`,
+
   }));
 
   return (
-    <nav
-      className={cn(
-        'flex-1 flex justify-center gap-x-4',
-        'text-sm font-medium text-neutral-500'
-      )}
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
+    <>
+      {/* Mobile Navbar */}
+      <div className="sm:hidden flex-1 justify-center text-left text-[25px] items-center mt-2 ml-2">
+        <button
+          onClick={toggleMenu}
+          className="text-neutral-500 hover:text-black"
+        >
+          {open ? <GrClose /> : <GrMenu />}
+        </button>
+
+        <nav
           className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
+            'flex flex-col bg-white absolute p-5 top-16 left-1/2 w-full transform -translate-x-1/2 z-50',
+            { 'hidden': !open, 'flex': open },
           )}
         >
-          {route.label}
-      </Link>
-      ))}
-    </nav>
-  )
+         
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+            >
+              <div
+                onClick={toggleMenu}
+                className={cn(
+                  'block py-2 px-4 text-right transition-colors hover:text-black',
+                  route.active ? 'text-black' : 'text-neutral-500'
+                )}
+              >
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </nav>
+      </div>
+
+      {/* Desktop Navbar */}
+      <nav
+        className={cn(
+          'hidden sm:flex flex-1 justify-center gap-x-4',
+          'text-sm font-medium text-neutral-500'
+        )}
+      >
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              'text-sm font-medium transition-colors hover:text-black',
+              route.active ? 'text-black' : 'text-neutral-500'
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </nav>
+    </>
+  );
 };
 
 export default MainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <Container>
-        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="mr-4 flex lg:mr-0 gap-x-2">
+        <div className="px-4 sm:px-6 lg:px-8 flex items-center h-16">
+          <Link href="/" className="mr-4 flex lg:mr-0 gap-x-2 items-center">
             <p className="font-bold text-xl">SunFire</p>
           </Link>
           <MainNav data={categories} />
