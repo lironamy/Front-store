4f9a6e357e464e89267cee754d7e3c0945ba9c57
Liron Avraham <lironamy@gmail.com>
2023-11-22 12:21:28 +0200

    added mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,10 +5,12 @@ import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
-import logo from "../app/logo.png";
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = async () => {
   const categories = await getCategories();
+
 
 
   return ( 
@@ -16,8 +18,9 @@ const Navbar = async () => {
       <Container>
         <div className="flex justify-around items-center h-16 p-4 ">
           <MainNav data={categories} />
-          <Link href="/" className="ml-8 flex lg:ml-0 gap-x-2 items-center flex-1 logoHd">
-            <Image src={logo} alt="logo" className="w-20 h-20 mt-5 z-100" />
+          <Link href="/" className="ml-16 flex lg:ml-0 gap-x-1 items-center flex-1 ">
+            <p className="font-bold text-xl">SunFire </p>
+            <FontAwesomeIcon icon={faMotorcycle} className="w-10 h-10" />
           </Link>
           <NavbarActions />
         </div>
