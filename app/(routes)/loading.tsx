0ccdbc38878b3b55a284b9734285b7901c4b772e
Liron Avraham <lironamy@gmail.com>
2023-11-24 14:21:59 +0200

    changed UI products cards

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
     <Container>
       <div className="w-full h-full p-8">
         <Skeleton className="w-full aspect-square rounded-xl md:aspect-[2.4/1]" />
-        <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="mt-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <Skeleton className="aspect-square rounded-xl" />
           <Skeleton className="aspect-square rounded-xl" />
           <Skeleton className="aspect-square rounded-xl" />
